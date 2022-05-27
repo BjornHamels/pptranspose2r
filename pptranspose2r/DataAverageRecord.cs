8deb3b3f54caf9c0e7b9117d5b09ed8cc4b560dd
Björn Hamels <873060@fontys.nl>
2022-05-27 15:05:36 +0200

    Data records from the example csv.

---
+++ pptranspose2r/DataAverageRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pptranspose2r
+{
+    /// <summary>
+    /// At the end of the horizontal line PP adds a summary/average record defined below.
+    /// </summary>
+    public record DataAverageRecord(decimal WakingWearTime_Avg,
+                                    decimal SittingTime_Avg,
+                                    decimal StandingTime_Avg,
+                                    decimal TotalSteppingTime_Avg,
+                                    decimal LightSteppingTime_Avg,
+                                    decimal MVPAsteppingTime_Avg,
+                                    int NumberOfSittingBouts_Avg,
+                                    int NumberOfSteps_Avg,
+                                    int Sitting0_30_Avg,
+                                    int Sitting30_60_Avg,
+                                    int Sitting60plus_Avg,
+                                    decimal Sitting0_30_t_Avg,
+                                    decimal Sitting30_60_t_Avg,
+                                    decimal Sitting60plus_t_Avg,
+                                    decimal WeartimeDays)
+    {
+
+    }
+}
---
+++ pptranspose2r/DataRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pptranspose2r
+{
+    /// <summary>
+    /// The data record that is repeated horizontally.
+    /// </summary>
+    public record DataRecord(string Date,
+                             int Sitting0_30,
+                             int Sitting30_60,
+                             int Sitting60plus,
+                             decimal WakingWearTime,
+                             decimal SittingTime,
+                             decimal StandingTime,
+                             decimal TotalSteppingTime,
+                             decimal LightSteppingTime,
+                             decimal MVPAsteppingTime,
+                             int NumberOfSittingBouts,
+                             int NumberOfSteps,
+                             decimal Sitting0_30_t,
+                             decimal Sitting30_60_t,
+                             decimal Sitting60plus_t)
+    {
+    }
+}
