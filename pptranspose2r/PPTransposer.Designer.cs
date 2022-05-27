caf63ab10ce8264c314630e5a7df8dff6b9a1d06
Björn Hamels <873060@fontys.nl>
2022-05-27 15:22:22 +0200

    Finished the UI.

--- pptranspose2r/PPTransposer.Designer.cs
+++ pptranspose2r/PPTransposer.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btLoadPPcsv = new System.Windows.Forms.Button();
+            this.ofdCSVKeuze = new System.Windows.Forms.OpenFileDialog();
+            this.lbInputFile = new System.Windows.Forms.Label();
+            this.lbOutputFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btLoadPPcsv
+            // 
+            this.btLoadPPcsv.Location = new System.Drawing.Point(12, 42);
+            this.btLoadPPcsv.Name = "btLoadPPcsv";
+            this.btLoadPPcsv.Size = new System.Drawing.Size(303, 87);
+            this.btLoadPPcsv.TabIndex = 0;
+            this.btLoadPPcsv.Text = "Load PP .csv";
+            this.btLoadPPcsv.UseVisualStyleBackColor = true;
+            this.btLoadPPcsv.Click += new System.EventHandler(this.btLoadPPcsv_Click);
+            // 
+            // ofdCSVKeuze
+            // 
+            this.ofdCSVKeuze.DefaultExt = "csv";
+            this.ofdCSVKeuze.FileName = "*.csv";
+            this.ofdCSVKeuze.Filter = "PP csv bestanden|*.csv";
+            this.ofdCSVKeuze.InitialDirectory = "./";
+            this.ofdCSVKeuze.Title = "Kies PP csv bestand";
+            // 
+            // lbInputFile
+            // 
+            this.lbInputFile.AutoSize = true;
+            this.lbInputFile.Location = new System.Drawing.Point(338, 42);
+            this.lbInputFile.Name = "lbInputFile";
+            this.lbInputFile.Size = new System.Drawing.Size(113, 32);
+            this.lbInputFile.TabIndex = 1;
+            this.lbInputFile.Text = "Open file";
+            // 
+            // lbOutputFile
+            // 
+            this.lbOutputFile.AutoSize = true;
+            this.lbOutputFile.Location = new System.Drawing.Point(338, 97);
+            this.lbOutputFile.Name = "lbOutputFile";
+            this.lbOutputFile.Size = new System.Drawing.Size(551, 32);
+            this.lbOutputFile.TabIndex = 2;
+            this.lbOutputFile.Text = "Loading the file instantly saves the processed data";
+            // 
+            // PPTransposer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1456, 192);
+            this.Controls.Add(this.lbOutputFile);
+            this.Controls.Add(this.lbInputFile);
+            this.Controls.Add(this.btLoadPPcsv);
+            this.Name = "PPTransposer";
             this.Text = "PPTransposer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btLoadPPcsv;
+        private OpenFileDialog ofdCSVKeuze;
+        private Label lbInputFile;
+        private Label lbOutputFile;
     }
 }
--- pptranspose2r/PPTransposer.cs
+++ pptranspose2r/PPTransposer.cs
@@ -16,5 +16,22 @@ namespace pptranspose2r
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// This is where the magic happens!
+        /// </summary>
+        private void btLoadPPcsv_Click(object sender, EventArgs e)
+        {
+            var result = ofdCSVKeuze.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = Path.GetDirectoryName(ofdCSVKeuze.FileName);
+                string inputFile = ofdCSVKeuze.FileName;
+                string participantID = "participantID";
+                string outputFile = path + $"\\{participantID}_pptranspose2r.csv";
+                lbInputFile.Text = inputFile;
+                lbOutputFile.Text = outputFile;
+            }
+        }
     }
 }
