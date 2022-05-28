3f71d2f51d07f1922c068485ea2c04c72d53d935
Björn Hamels <873060@fontys.nl>
2022-05-28 11:08:46 +0200

    Added a ctor for the unparsed string data. Changed the datatype to decimal instead of int.

--- pptranspose2r/DataAverageRecord.cs
+++ pptranspose2r/DataAverageRecord.cs
@@ -8,6 +8,8 @@ namespace pptranspose2r
 {
     /// <summary>
     /// At the end of the horizontal line PP adds a summary/average record defined below.
+    /// Eventhough the record in the test data shows interger rounding for some fields,
+    /// decimals are chosen to store the numbers due to their accuracy.
     /// </summary>
     public record DataAverageRecord(decimal WakingWearTime_Avg,
                                     decimal SittingTime_Avg,
@@ -15,16 +17,52 @@ namespace pptranspose2r
                                     decimal TotalSteppingTime_Avg,
                                     decimal LightSteppingTime_Avg,
                                     decimal MVPAsteppingTime_Avg,
-                                    int NumberOfSittingBouts_Avg,
-                                    int NumberOfSteps_Avg,
-                                    int Sitting0_30_Avg,
-                                    int Sitting30_60_Avg,
-                                    int Sitting60plus_Avg,
+                                    decimal NumberOfSittingBouts_Avg,
+                                    decimal NumberOfSteps_Avg,
+                                    decimal Sitting0_30_Avg,
+                                    decimal Sitting30_60_Avg,
+                                    decimal Sitting60plus_Avg,
                                     decimal Sitting0_30_t_Avg,
                                     decimal Sitting30_60_t_Avg,
                                     decimal Sitting60plus_t_Avg,
                                     decimal WeartimeDays)
     {
 
+        /// <summary>
+        /// Constructor used when reading the strings unparsed from file.
+        /// </summary>
+        public DataAverageRecord(string wakingWearTime_Avg,
+                                 string sittingTime_Avg,
+                                 string standingTime_Avg,
+                                 string totalSteppingTime_Avg,
+                                 string lightSteppingTime_Avg,
+                                 string mVPAsteppingTime_Avg,
+                                 string numberOfSittingBouts_Avg,
+                                 string numberOfSteps_Avg,
+                                 string sitting0_30_Avg,
+                                 string sitting30_60_Avg,
+                                 string sitting60plus_Avg,
+                                 string sitting0_30_t_Avg,
+                                 string sitting30_60_t_Avg,
+                                 string sitting60plus_t_Avg,
+                                 string weartimeDays) : this(Convert.ToDecimal(wakingWearTime_Avg),
+                                                             Convert.ToDecimal(sittingTime_Avg),
+                                                             Convert.ToDecimal(standingTime_Avg),
+                                                             Convert.ToDecimal(totalSteppingTime_Avg),
+                                                             Convert.ToDecimal(lightSteppingTime_Avg),
+                                                             Convert.ToDecimal(mVPAsteppingTime_Avg),
+                                                             Convert.ToDecimal(numberOfSittingBouts_Avg),
+                                                             Convert.ToDecimal(numberOfSteps_Avg),
+                                                             Convert.ToDecimal(sitting0_30_Avg),
+                                                             Convert.ToDecimal(sitting30_60_Avg),
+                                                             Convert.ToDecimal(sitting60plus_Avg),
+                                                             Convert.ToDecimal(sitting0_30_t_Avg),
+                                                             Convert.ToDecimal(sitting30_60_t_Avg),
+                                                             Convert.ToDecimal(sitting60plus_t_Avg),
+                                                             Convert.ToDecimal(weartimeDays))
+        {
+
+        }
     }
+
 }
