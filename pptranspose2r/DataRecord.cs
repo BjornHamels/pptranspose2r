165fab1eca720fc9ee4f92c577019ec72ee710ff
Björn Hamels <873060@fontys.nl>
2022-05-28 10:59:37 +0200

    Added a ctor for the unparsed string data.

--- pptranspose2r/DataRecord.cs
+++ pptranspose2r/DataRecord.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace pptranspose2r
 {
     /// <summary>
-    /// The data record that is repeated horizontally.
+    /// The data record that is repeated horizontally by the PP tool.
     /// </summary>
     public record DataRecord(string Date,
                              int Sitting0_30,
@@ -26,11 +26,68 @@ namespace pptranspose2r
                              decimal Sitting30_60_t,
                              decimal Sitting60plus_t) : IComparable<DataRecord>
     {
+
+        /// <summary>
+        /// Constructor used when reading the strings unparsed from file.
+        /// </summary>
+        public DataRecord(string date,
+                          string sitting0_30,
+                          string sitting30_60,
+                          string sitting60plus,
+                          string wakingWearTime,
+                          string sittingTime,
+                          string standingTime,
+                          string totalSteppingTime,
+                          string lightSteppingTime,
+                          string mVPAsteppingTime,
+                          string numberOfSittingBouts,
+                          string numberOfSteps,
+                          string sitting0_30_t,
+                          string sitting30_60_t,
+                          string sitting60plus_t) : this(date,
+                                                         Convert.ToInt32(sitting0_30),
+                                                         Convert.ToInt32(sitting30_60),
+                                                         Convert.ToInt32(sitting60plus),
+                                                         Convert.ToDecimal(wakingWearTime),
+                                                         Convert.ToDecimal(sittingTime),
+                                                         Convert.ToDecimal(standingTime),
+                                                         Convert.ToDecimal(totalSteppingTime),
+                                                         Convert.ToDecimal(lightSteppingTime),
+                                                         Convert.ToDecimal(mVPAsteppingTime),
+                                                         Convert.ToInt32(numberOfSittingBouts),
+                                                         Convert.ToInt32(numberOfSteps),
+                                                         Convert.ToDecimal(sitting0_30_t),
+                                                         Convert.ToDecimal(sitting30_60_t),
+                                                         Convert.ToDecimal(sitting60plus_t))
+
+        {
+
+        }
+
+
         /// <summary>
         /// For parsing the Date field (string).
         /// </summary>
         private static string DatePattern = @"([0-9]{1,2})-([0-9]{1,2})-([0-9]{4})";
 
+        /// <summary>
+        /// The header texts.
+        /// </summary>
+        private readonly string[] DataHeaders = new string[] {"Date",
+                                                              "Sitting0_30",
+                                                              "Sitting30_60",
+                                                              "Sitting60+",
+                                                              "Waking wear time",
+                                                              "Sitting time",
+                                                              "Standing time",
+                                                              "Total stepping time",
+                                                              "Light stepping time",
+                                                              "MVPA stepping time",
+                                                              "Number of sitting bouts",
+                                                              "Number of steps",
+                                                              "Sitting0_30_t",
+                                                              "Sitting30_60_t",
+                                                              "Sitting60+_t"};
 
         /// <summary>
         /// Returns a true DateOnly object of this records Date field.
